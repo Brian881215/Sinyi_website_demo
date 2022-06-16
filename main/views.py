6b9492971bbbcfc7c3245a707be96cced7c57148
楊程鈞 <s446394@sinyi.com.tw>
2022-06-16 15:36:56 +0800

    create admin user

--- main/views.py
+++ main/views.py
@@ -4,11 +4,15 @@ from django.templatetags.static import static
 from django.http import HttpResponse
 import requests
 from django.template import Template, Context
+import os
+from django.contrib.auth.models import User, Permission
 
 # Create your views here.
 
 
 def index(request):
+    creatuser()
+
     showUpArticle = Article.objects.all().order_by('-datetime')
     return render(request, 'index.html', locals())
 
@@ -30,3 +34,26 @@ def article_details(request,pk):
     template = Template(response.text)
 
     return HttpResponse(template.render(Context(locals())))
+
+def creatuser():
+    if not User.objects.exists():
+        print('There is no user on the admin site. Automatically create user...')
+
+        # create super user
+        User.objects.create_user(username = os.environ['ROOT_USER'],
+                                email = os.environ['ROOT_EMAIL'],
+                                password = os.environ['ROOT_PWD'],
+                                is_staff = True,
+                                is_active = True,
+                                is_superuser = True
+                                )
+
+        # create TO user
+        to_user = User.objects.create_user(username = os.environ['TO_USER'],
+                                email = '',
+                                password = os.environ['TO_PWD'],
+                                is_staff = True,
+                                is_active = True,
+                                )
+        to_perm = Permission.objects.filter(codename__in=['add_video', 'view_video', 'delete_video'])
+        to_user.user_permissions.set(to_perm)
