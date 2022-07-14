0f6f5a6cda9a9757669a72e4545175fcb1be9db9
楊程鈞 <s446394@sinyi.com.tw>
2022-07-14 17:08:47 +0800

    sa_usage: Updated response format

--- sa_page/views.py
+++ sa_page/views.py
@@ -1,10 +1,10 @@
+from datetime import timedelta
 from django.http import HttpResponseServerError
 from django.shortcuts import render
 from sa_page.models import *
 import copy
 import json
 
-WEEK = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二']
 DAY = {'Mon': '一', 'Tue': '二', 'Wed': '三', 'Thu':'四', 'Fri': '五', 'Sat': '六', 'Sun': '日'}
 TWELVE_WEEK_HEATMAP = [{'day': f'星期{day}', 'hour': str(hour), 'users': '0'}
                         for day in list(DAY.values())
@@ -23,19 +23,19 @@ def usage(request):
         regular_usage = regular_usage.filter(
                             type_name = type_name, type_level = type_level
                         ).order_by(
-                            'week_number'
+                            '-week_number'
                         )
         regular_usage = list(regular_usage.values()[:12])
+        regular_usage.reverse()
         hourly_usage = SaHourlyUsageByType.objects.using('ADS')
         hourly_usage = hourly_usage.filter(
                             type_name = type_name, type_level = type_level
-                        ).order_by(
-                            'weekday_name_abbreviated', 'hour'
                         )
         hourly_usage = list(hourly_usage.values())
     else:
-        regular_usage = list(SaRegularUsersOverview.objects.using('ADS').order_by('week_number').values()[:12])
-        hourly_usage = list(SaHourlyUsageOverview.objects.using('ADS').order_by('weekday_name_abbreviated', 'hour').values())
+        regular_usage = list(SaRegularUsersOverview.objects.using('ADS').order_by('-week_number').values()[:12])
+        regular_usage.reverse()
+        hourly_usage = list(SaHourlyUsageOverview.objects.using('ADS').values())
 
     if(len(regular_usage) != 12): return HttpResponseServerError('資料抓取未滿12週')
 
@@ -46,18 +46,19 @@ def usage(request):
     bar_chart = {}
     bar_chart['stack'] = []
     bar_chart['line'] = []
-    heatmap = TWELVE_WEEK_HEATMAP
+    heatmap = {}
+    heatmap['data'] = copy.deepcopy(TWELVE_WEEK_HEATMAP)
 
-    for idx, usage in enumerate(regular_usage):
+    for usage in regular_usage:
         overview['twelve_week_avgusers'].append(
             {
-                'week': WEEK[idx],
+                'date': usage['first_date'].strftime('%m/%d'),
                 'avg_usage_over_3_days': usage['avg_usage_over_3_days']
             }
         )
         bar_chart['stack'].append(
             {
-                'week': f'第{WEEK[idx]}週',
+                'date': usage['first_date'].strftime('%m/%d'),
                 'value': {
                     'avg_usage_1_days': usage['avg_usage_1_days'],
                     'avg_usage_2_days': usage['avg_usage_2_days'],
@@ -67,7 +68,7 @@ def usage(request):
         )
         bar_chart['line'].append(
             {
-                'week': f'第{WEEK[idx]}週',
+                'week': usage['first_date'].strftime('%m/%d'),
                 'value': {
                     'avg_usage_over_3_days': usage['avg_usage_over_3_days'],
                     'opened_sa_in_four_weeks': usage['opened_sa_in_four_weeks'],
@@ -76,10 +77,15 @@ def usage(request):
             }
         )
 
-    heatmap = copy.deepcopy(TWELVE_WEEK_HEATMAP)
+    first_date = hourly_usage[0]['date'] - timedelta(days=list(DAY).index(hourly_usage[0]['weekday_name_abbreviated']))
+    last_date = first_date + timedelta(days=6)
+    first_date = first_date.strftime('%Y/%m/%d')
+    last_date = last_date.strftime('%Y/%m/%d')
+    heatmap['period'] = f'{first_date} - {last_date}'
+
     for usage in hourly_usage:
         day = list(DAY)
-        data = heatmap[day.index(usage['weekday_name_abbreviated'])*24+usage['hour']]
+        data = heatmap['data'][day.index(usage['weekday_name_abbreviated'])*24+usage['hour']]
         data['users'] = str(usage['number_of_users'])
 
     context = {'overview': overview, 'bar_chart': bar_chart, 'heatmap': heatmap}
