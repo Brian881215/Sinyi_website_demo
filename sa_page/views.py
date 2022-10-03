a64a389382378f14b7968262fca733a5cb4f3f74
劉羽涵 <tammy.liou@sinyi.com.tw>
2022-10-03 16:10:10 +0800

    新增客戶標籤應用與案源購物車

--- sa_page/views.py
+++ sa_page/views.py
@@ -126,13 +126,12 @@ def sellingprocess(request):
 def events(request):
     return render(request, 'events.html')
 
-<<<<<<< HEAD
+
 def customertagfilter(request):
     return render(request, 'customertagfilter.html')
 
 def productcart(request):
-    return render(request, 'productcart.com')
-=======
+    return render(request, 'productcart.html')
+
 def sa_base(request):
     return render(request, 'sa_base.html')
->>>>>>> goldenTagRule
